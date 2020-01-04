4a59e1ec8508ee1ce2c6b68435c785d654af669e
pasdam <pasdam@users.noreply.github.com>
2020-01-04 10:26:03 +0800

    Backup local stash

---
+++ env.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+export QT_API=5.13.0
+export QT_DIR=/Users/paco/programs/Qt
+export QT_VERSION=5.13.1
+export CGO_CFLAGS_ALLOW=.*
+export CGO_CXXFLAGS_ALLOW=.*
+export CGO_LDFLAGS_ALLOW=.*
+
+export QT_DEBUG=true
+export QT_DEBUG_CPP=true
